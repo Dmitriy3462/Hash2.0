19376b9f6c88389a6cacdfd301caa52b8901aef2
laimz <laimzcash@gmail.com>
2023-04-24 21:45:32 +0300

    Добавьте файлы проекта.

---
+++ Block.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hash
+{
+     class Block
+    {
+        public int id {  get; private set; }
+        public string Data { get; private set; }
+        public DateTime Created { get; private set; }
+        public string Hash {get; private set; }
+        public string PreviousHash { get; private set; }
+        public string User { get; set; }
+        
+        
+        public Block()
+        {
+            id = 1;
+            Data = "Hash";
+            Created = DateTime.Parse("24.04.2024 00:00:00.000").ToLocalTime();
+            PreviousHash = "11111";
+            User = "Dmitriy";
+        }
+        public Block(string data, Block block)
+        {
+            if (string.IsNullOrEmpty(data))
+            {
+                throw new ArgumentNullException($"Empty data", nameof(data));
+            }
+
+            if (block == null)
+            {
+                throw new ArgumentNullException("Empty Argument Block", nameof(block));
+            }
+
+
+        }
+    }
+}
